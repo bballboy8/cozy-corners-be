8efdec56af6149ec928bc87a809fda55ac991755
ChiragFullstack <chiragmahajan9019@gmail.com>
2024-12-28 22:58:21 +0530

    Bearer token

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,12 +4,13 @@ const authenticate = (req, res, next) => {
   try {
     // Check if the 'Authorization' header is present
     const authHeader = req.headers['authorization'];
-    if (!authHeader || !authHeader.toLowerCase().startsWith('bearer ')) {
-      return res.status(401).json({ message: 'Unauthorized access' });
-    }
-
-    // Extract the token from the header
-    const token = authHeader.split(' ')[1];
+    // if (!authHeader || !authHeader.toLowerCase().startsWith('bearer ')) {
+    //   return res.status(401).json({ message: 'Unauthorized access' });
+    // }
+    const token = getTokenFromHeader(authHeader);
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized access' }); // Generic error message
+    }  
 
     // Verify the token using jwt.verify
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
@@ -26,6 +27,13 @@ const authenticate = (req, res, next) => {
     console.error('Authentication error:', error);
     return res.status(500).json({ message: 'Internal server error' });
   }
+};
+
+const getTokenFromHeader = (authHeader) => {
+  if (!authHeader || !authHeader.toLowerCase().startsWith('bearer ')) {
+    return null; // Return null if the header is invalid
+  }
+  return authHeader.split(' ')[1]; // Extract and return the token
 };
 
 module.exports = authenticate;
